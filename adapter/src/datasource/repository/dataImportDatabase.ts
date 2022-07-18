ae105306ac13bd435033b13ec4488292a9f92f40
MarcoDoell <marco.doell96@gmail.com>
2022-07-18 23:52:42 +0200

    zurück geabaut

--- adapter/src/datasource/repository/dataImportDatabase.ts
+++ adapter/src/datasource/repository/dataImportDatabase.ts
@@ -12,11 +12,11 @@ import {
 import { createDataImportTable } from './dataImportRepository';
 
 const POOL_CONFIG: PoolConfig = {
-  host: "localhost",
-  port: 5432 as number,
-  user: "adapterservice",
-  password: "admin",
-  database: "adapterservice",
+  host: POSTGRES_HOST,
+  port: POSTGRES_PORT as unknown as number,
+  user: POSTGRES_USER,
+  password: POSTGRES_PW,
+  database: POSTGRES_DB,
   max: 20,
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 2000,
